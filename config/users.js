4c4237a49b7b5c837abd1b8f77afa167bc1b3e2b
hoanglong <ro1462000@gmail.com>
2023-01-03 10:21:57 +0700

    add

---
+++ config/users.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    name: 'Long',
+    lastName: 'Le',
+    statusText: 'I am the leader!',
+    image: 'users/rick.jpg'
+  },
+  {
+    name: 'Tài',
+    lastName: 'Mập',
+    statusText: 'I like smashing Walkers.',
+    image: 'users/daryl.jpg'
+  },
+  {
+    name: 'Dương',
+    lastName: 'Nguyễn',
+    statusText: 'Don\'t mess with me!',
+    image: 'users/carol.jpg'
+  },
+  {
+    name: 'Mạnh',
+    lastName: '',
+    statusText: 'In a relationship with Lucille.',
+    image: 'users/negan.jpeg'
+  }
+]
+
